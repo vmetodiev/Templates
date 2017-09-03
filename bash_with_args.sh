a03fb26b956f2023d783bf57c0b345e946e7f9f8
Varban Metodiev <varban.metodiev@gmail.com>
2017-09-03 16:18:44 +0200

    Initial commit

---
+++ bash_with_args.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Bash script with argument parsing skeleton
+# With some help from: http://www.bahmanm.com/blogs/command-line-options-how-to-parse-in-bash-using-getopt
+
+function usage(){
+        echo "Usage: $0 [-a|--arga <value>] [-b|--argb <value>]"
+}
+
+####################################################################################
+
+# “a” and “arga” have required arguments.
+# “b” and “argb” have required arguments.
+
+# read the options - in this case 'a|--arga' and '-b|--argb' are required
+TEMP=`getopt -o a:b: --long arga:,argb: -n "$0.sh" -- "$@"`
+eval set -- "$TEMP"
+
+# check if args entered
+if [ -z "$2" ]; then usage; exit 0; fi
+
+# extract options and their arguments into variables.
+while true ; do
+    case "$1" in
+        -a|--arga)
+            case "$2" in
+                "") shift 2 ;;
+                *) ARG_A=$2 ; shift 2 ;;
+            esac ;;
+        -b|--argb)
+            case "$2" in
+                "") shift 2 ;;
+                *) ARG_B=$2 ; shift 2 ;;
+            esac ;;
+        # and so on for agrc and successors...
+	--) shift ; break ;;
+        *) echo "Internal error!" ; exit 1 ;;
+    esac
+done
+
+#####################################################################################
+
+# Print parsed result
+echo "ARG_A = $ARG_A"
+echo "ARG_B = $ARG_B"
